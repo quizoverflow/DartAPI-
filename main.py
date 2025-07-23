128bc171f050987640e8843e5f9cdb20607cd9b9
jinuchoi <jinu010219@naver.com>
2025-07-23 22:30:00 +0900

    .

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tool import *
 from data_controller import DataController
 from tqdm import tqdm
 import OpenDartReader
+import openpyxl
 
 @singleton
 class ReportCrawler():
@@ -12,9 +13,18 @@ class ReportCrawler():
         #api key등이 담긴 data/meta.json 파일을 불러옵니다
         meta_data = DataController().get_meta_data()
         self.api_key = meta_data["api_key"]
-        
+
         #OpenDartReader 객체 생성
         self.dart = OpenDartReader(self.api_key)
 
-        
+        test = self.dart.finstate('삼성전자', 2021)
+        test.to_excel(excel_writer = 'data/samsung_test.xlsx')
+
+def main():
+    reportCrawler =ReportCrawler()
+
+
+
+if __name__ == "__main__":
+    main()
 
